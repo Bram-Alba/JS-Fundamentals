d1672d0c0c3d9986a7afb7e23f949321fb73b9a0
Bram-Alba <abrahamabaya002@gmail.com>
2025-11-10 14:42:28 +0100

    Fix 7-multi_c.js syntax and handle negative numbers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
-const x = parseInt(process.argv[2]);
+const times = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
-  console.log("Missing number of occurrences");
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
   }
 }
+
 
