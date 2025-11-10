92fd80cffbbf09d9a7d89db3f828666343af0eb4
Bram-Alba <abrahamabaya002@gmail.com>
2025-11-10 15:48:16 +0100

    Fix 1-multi_languages.js to use array and loop for correct output

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,5 @@
-
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const msg of messages) {
+  console.log(msg);
+}
 
